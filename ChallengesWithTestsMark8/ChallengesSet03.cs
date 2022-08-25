07cf9e7bf0df493d354d12241dde2938a5536894
Nancy Lee <107009879+nancylee1@users.noreply.github.com>
2022-08-24 20:43:59 -0400

    Challenge set 4

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -11,46 +11,50 @@ namespace ChallengesWithTestsMark8
             foreach (var item in vals)
             {
                 if (item == false)
-                    return true;                
+                    return true;
             }
             return false;
+
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            int sum = 0;
+            //int sum = 0;
 
-            if (numbers == null)
-            {
-                return false; 
-            }
-            foreach (var item in numbers)
-            {
-                sum += item;
-            }
-                if (sum % 2 != 0)
-                {
-                return true;
-                }
-                else return false;
+            //if (numbers == null)
+            //{
+            //    return false; 
+            //}
+            //foreach (var item in numbers)
+            //{
+            //    sum += item;
+            //}
+            //    if (sum % 2 != 0)
+            //    {
+            //    return true;
+            //    }
+            //    else return false;
+            return numbers != null && numbers.Where(x => x % 2 != 0).Sum() % 2 != 0; 
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            var containsUpLowNum = password.Any(char.IsUpper) && password.Any(char.IsLower) && password.Any(char.IsNumber) ? true : false; 
-            return containsUpLowNum;
+            return password.Any(char.IsUpper) && password.Any(char.IsLower) && password.Any(char.IsNumber) ? true : false; 
+
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            var firstLetter = val[0];
-            return firstLetter;
+            //var firstLetter = val[0];
+            //return firstLetter;
+            return val[0];
         }
 
         public char GetLastLetterOfString(string val)
         {
             var lastLetter = val[val.Length - 1];
             return lastLetter;
+            //return val[^1];
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
@@ -75,16 +79,29 @@ namespace ChallengesWithTestsMark8
                     list.Add(i);
                 }
             }
-            return list.ToArray();  
+            return list.ToArray();
+
+
+            //int[] result = new int[50];
+            //int index = 0;
+
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        result[index] = i; 
+            //        index++;
+            //    }
+            //}
+            //return result;
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
             for (int i = 0; i < words.Length; i++)
             {
-                words[i] = words[i].ToUpper(); 
+                words[i] = words[i].ToUpper();
             }
-
         }
     }
 }
--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,47 +8,79 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            return numbers.Where(n => n % 2 == 0).Sum() - numbers.Where (n =>n % 2 != 0).Sum();
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            var lengthOfShortest = new List<int>() { str1.Length, str2.Length, str3.Length, str4.Length };
+            return lengthOfShortest.Min();
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            var smallestNumber = new List<int>() { number1, number2, number3, number4 };    
+            return smallestNumber.Min();
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            // Triangles must have: 2 sides added together is larger than the third
+            return sideLength1 + sideLength2 > sideLength3 && sideLength2 + sideLength3 > sideLength1 && sideLength3 + sideLength1 > sideLength2;            
         }
 
         public bool IsStringANumber(string input)
-        {
-            throw new NotImplementedException();
+        {   double num = 0;
+            return double.TryParse(input, out num);
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            return objs.Where(x => x == null).Count() > (objs.Count() / 2);
         }
 
         public double AverageEvens(int[] numbers)
+        //{
+        //    if (numbers == null || numbers.Count() == 0)
+        //    {
+        //        return 0; 
+        //    }
+        //    var evens = numbers.Where(x => x % 2 == 0);
+
+        //    if (evens.Count() == 0)
+        //    {
+        //        return 0;
+        //    }
+        //    else
+        //        return evens.Average(); 
+        //}
         {
-            throw new NotImplementedException();
+            return numbers == null || numbers.Length == 0 ? 
+                0 :numbers.Where(x => x % 2 == 0).Count() == 0 ? 
+                0 : numbers.Where(x => x % 2 == 0).Average();    
         }
 
         public int Factorial(int number)
+        //{
+        //    if (number < 0)
+        //    {
+        //        throw new ArgumentOutOfRangeException();
+        //    }
+        //    if (number == 0)
+        //    {
+        //        return 1;
+        //    }
+        //    return number * Factorial(number - 1);
+
+        //}
         {
-            throw new NotImplementedException();
+            return number == 0 ? 1 : number * Factorial(number - 1);         
         }
+            
     }
 }
